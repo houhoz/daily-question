9289c2e969501565b1418143f56094270d13c46a
yazhao.hou <yazhao.hou@upai.com>
2021-03-01 11:17:46 +0800

    feat: 2

---
+++ 2.js
@@ -0,0 +1,24 @@
+Object.prototype.map = function (handleFn, thisValue) {
+  const obj = this;
+  let res = {};
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      res[prop] = handleFn.call(thisValue, obj[prop], prop, obj);
+    }
+  }
+  return res;
+};
+// 测试用例
+var obj = {
+  name: "sunny",
+  sex: "man",
+};
+var res = obj.map(
+  function (val, prop, obj) {
+    console.log(this);
+    console.log(val, prop, obj);
+    return prop + "--" + val;
+  },
+  { name: "thisthis" }
+);
+console.log("res:", res);
